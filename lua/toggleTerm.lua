6c45495d879ff053c09f1de08c9b9b4e18e5dfb7
Peter Wilhelmsson <2hdddg@gmail.com>
2023-04-30 14:52:52 +0200

    Support for multiple terminals

--- lua/toggleTerm.lua
+++ lua/toggleTerm.lua
@@ -1,8 +1,18 @@
 local M = {}
 -- Shared between windows and buffers, only one terminal instance
-local terminalBuf = nil
+local terminalBufs = {}
 
-M.toggle = function()
+local function is_terminal_buf(buf)
+    for i, b in ipairs(terminalBufs) do
+        if b == buf then
+            return true
+        end
+    end
+    return false
+end
+
+M.to_terminal = function(index)
+    local terminalBuf = terminalBufs[index]
     local init = false
     if terminalBuf == nil or (not vim.api.nvim_buf_is_valid(terminalBuf)) then
         -- Need to initialize the terminal in a real windows
@@ -11,30 +21,36 @@ M.toggle = function()
         vim.api.nvim_buf_set_option(terminalBuf, "buftype", "nofile")
         vim.api.nvim_buf_set_option(terminalBuf, "filetype", "myterm")
         init = true
+        terminalBufs[index] = terminalBuf
     end
     local buf = vim.api.nvim_get_current_buf()
     local win = vim.api.nvim_get_current_win()
-    if buf == terminalBuf then
-        -- Switch back to the buffer that was previously active
-        -- in the current window.
-        local previousBuf = vim.w["TT:buf"]
-        if previousBuf == nil then
-            return
-        end
-        if not vim.api.nvim_buf_is_valid(previousBuf) then
-            return
-        end
-        vim.api.nvim_win_set_buf(win, previousBuf)
-    else
-        -- Switch to terminal, stash away the buffer that was active
-        -- in this window.
+    if not is_terminal_buf(buf) then
         vim.api.nvim_win_set_var(win, "TT:buf", buf)
-        vim.api.nvim_win_set_buf(win, terminalBuf)
-        if init then
-            vim.fn.termopen("/bin/bash")
-        end
-        vim.cmd("startinsert")
     end
+    vim.api.nvim_win_set_buf(win, terminalBuf)
+    if init then
+        vim.fn.termopen("/bin/bash")
+    end
+    vim.cmd("startinsert")
+end
+
+M.from_terminal = function()
+    local buf = vim.api.nvim_get_current_buf()
+    if not is_terminal_buf(buf) then
+        return
+    end
+    -- Switch back to the buffer that was previously active
+    -- in the current window.
+    local win = vim.api.nvim_get_current_win()
+    local previousBuf = vim.w["TT:buf"]
+    if previousBuf == nil then
+        return
+    end
+    if not vim.api.nvim_buf_is_valid(previousBuf) then
+        return
+    end
+    vim.api.nvim_win_set_buf(win, previousBuf)
 end
 
 M.setup = function()
